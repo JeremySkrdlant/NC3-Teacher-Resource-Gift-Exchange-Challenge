c339769d6b16d526cbe966766c35b2c75e962a11
Jeremy <jeremy.skrdlant@nwktc.edu>
2025-05-04 18:09:22 -0600

    Initial Commit

---
+++ Gift Exchange Challenge Trial/Gift_Exchange_Challenge_TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gift_Exchange_Challenge_TrialApp.swift
+//  Gift Exchange Challenge Trial
+//
+//  Created by admin on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Gift_Exchange_Challenge_TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
